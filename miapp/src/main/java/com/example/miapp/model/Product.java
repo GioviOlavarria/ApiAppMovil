2313e1041b210cff53c5b837dedc24a42d57ed18
Giovanni <gi.olavarria@duocuc.cl>
2025-12-01 15:23:59 -0300

    cambio de variable y agregado el puerto para despliegue en railway

--- miapp/src/main/java/com/example/miapp/model/Product.java
+++ miapp/src/main/java/com/example/miapp/model/Product.java
@@ -14,7 +14,7 @@ public class Product {
     private Long id;
 
     private String name; 
-    private String price; 
+    private Double price;
     private String description;
     private String category;
 
